f4a12938f0833bb374050ede4ce294556734eae6
jevin josh <jevinjosh000@gmail.com>
2025-01-27 09:50:09 +0530

    Complete virtual Assistant API task

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Login from "../components/Login/Login.jsx";
+
+
+
+
+const Loginpage = () => {
+    return (
+        <div>
+            <Login/>
+        </div>
+    )
+}
+export default Loginpage;
