a4e8d4cb77f4ffc38737b2ac501d5f74dfd7b96b
jevinjosh <jevinjosh000@gmail.com>
2025-02-19 16:03:38 +0530

    Delete frontend directory

--- frontend/src/routes/routes.js
+++
@@ -1,8 +0,0 @@
-import LoginPage from "../pages/LoginPage";
-import SignupPage from "../pages/SignupPage";
-import Home from"../pages/Home"
-import createProduct from "../pages/createProduct";
-import MyProducts from "../pages/myProduct";
-export{
-    LoginPage,SignupPage,Home,createProduct,MyProducts}
-    
