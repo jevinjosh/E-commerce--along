ecd82923e776446d66807b3fef9c9ac6250bd007
jevin josh <jevinjosh000@gmail.com>
2025-02-20 09:32:02 +0530

    update

--- frontend/src/components/myproducts.jsx
+++ frontend/src/components/myproducts.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-// import Nav from "../components/nav"
+//  import Nav from "../components/nav"
 
 
 function Myproduct({ _id, name, images, description, price }) {
